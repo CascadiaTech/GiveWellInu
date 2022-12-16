d6c670ff342ce9c409b5a5c71069240d55993e44
CascadiaTech <hollowaycryptoconsulting@gmail.com>
2022-12-16 13:27:28 -0800

    provider

--- components/Web3Modal/WalletConnect.tsx
+++ components/Web3Modal/WalletConnect.tsx
@@ -15,13 +15,13 @@ export const ConnectWallet = () => {
   });
   const walletconnect = new WalletConnectConnector({
     supportedChainIds: [1, 3, 4, 5, 42, 11155111],
-    rpc: { 1: `https://mainnet.infura.io/v3/e0171a3aab904c6bbe6622e6598770ad`, 5: `https://goerli.infura.io/v3/e0171a3aab904c6bbe6622e6598770ad` },
+    rpc: { 1: `https://mainnet.infura.io/v3/c686522a4d384ef28e376ec0406a424f`, 5: `https://goerli.infura.io/v3/c686522a4d384ef28e376ec0406a424f` },
     bridge: "https://bridge.walletconnect.org",
     qrcode: true,  
   });
 
   const CoinbaseWallet = new WalletLinkConnector({
-    url: `https://mainnet.infura.io/v3/e0171a3aab904c6bbe6622e6598770ad`,
+    url: `https://mainnet.infura.io/v3/c686522a4d384ef28e376ec0406a424f`,
     appName: "Web3",
     supportedChainIds: [1, 3, 4, 5, 42, 11155111],
   });
